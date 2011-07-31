6b713c647e03fa8ce41f587c68297e4c3d74bfc0
Noah Tye <noah@twitter.com>
2011-07-31 13:36:37 -0700

    use cristian's algorithm for clock syncing instead of latency tests.

--- server.js
+++ server.js
@@ -3,48 +3,103 @@ io = require('socket.io'),
 path = require('path'),
 paperboy = require('paperboy');
 
-var audioSrc = 'audio/test.mp3';
-var startTimeMax = 5000;
-var startTime = startTimeMax;
-var startTimeout;
-var countDownTimeout;
-var clients = [];
-var server = http.createServer(function(req, res) {
-    paperboy.deliver(path.dirname(__filename), req, res);
+var getNetworkIP = (function () {
+  var ignoreRE = /^(127\.0\.0\.1|::1|fe80(:1)?::1(%.*)?)$/i;
+
+  var exec = require('child_process').exec;
+  var cached;    
+  var command;
+  var filterRE;
+
+  switch (process.platform) {
+    // TODO: implement for OSs without ifconfig command
+  case 'darwin':
+    command = 'ifconfig';
+    filterRE = /\binet\s+([^\s]+)/g;
+    // filterRE = /\binet6\s+([^\s]+)/g; // IPv6
+    break;
+  default:
+    command = 'ifconfig';
+    filterRE = /\binet\b[^:]+:\s*([^\s]+)/g;
+    // filterRE = /\binet6[^:]+:\s*([^\s]+)/g; // IPv6
+    break;
+  }
+
+  return function (callback, bypassCache) {
+    // get cached value
+    if (cached && !bypassCache) {
+      callback(null, cached);
+      return;
+    }
+    // system call
+    exec(command, function (error, stdout, sterr) {
+      var ips = [];
+      // extract IPs
+      var matches = stdout.match(filterRE);
+      // JS has no lookbehind REs, so we need a trick
+      for (var i = 0; i < matches.length; i++) {
+        ips.push(matches[i].replace(filterRE, '$1'));
+      }
+
+      // filter BS
+      for (var i = 0, l = ips.length; i < l; i++) {
+        if (!ignoreRE.test(ips[i])) {
+          //if (!error) {
+          cached = ips[i];
+          //}
+          callback(error, ips[i]);
+          return;
+        }
+      }
+      // nothing found
+      callback(error, null);
     });
+  };
+})();
 
-server.listen(8080, '192.168.2.4');
+getNetworkIP(function (error, ip) {
+  console.log(ip);
+  if (error) {
+    console.log('error:', error);
+  } else {
+    runServer(ip);
+  }
+}, true);
 
-var socket = io.listen(server);
-socket.on('connection', function(client) {
-	      client.send({'src': audioSrc});
-	      clients.push(client);
-	      startTime = startTimeMax;
-	      clearTimeout(countDownTimeout);
-	      client.send({'latencyTest': new Date().getTime()});
-	      startCountDown();
-	      client.on('message', function(data) {
-			    if ('latencyTest' in data) {
-				client.latency = data.latencyTest;
-			    }
-			});
-	  });
-function start() {
-    var date = new Date();
-    var curtime = date.getTime();
-    var goTime = 5000 + curtime;
-    for (var i = 0; i < clients.length; i++) {
-	clients[i].send({'play': goTime - new Date().getTime() -
-			 clients[i].latencyTest});
-    }
-}
-console.log('awesome. it\'s up.');
-function startCountDown() {
-    startTime -= 1000;
-    console.log('t:'+startTime);
-    if (startTime > 0) {
-	countDownTimeout = setTimeout(startCountDown, 1000);
-    } else {
+function runServer(ip) {
+  console.log('ip = ', ip);
+  var audioSrc = 'audio/test.mp3';
+  var clients = [];
+  var server = http.createServer(function(req, res) {
+    paperboy.deliver(path.dirname(__filename), req, res);
+  });
+
+  server.listen(8080);
+
+  var socket = io.listen(server);
+  socket.on('connection', function(client) {
+    console.log('client connect.');
+    client.send({'src': audioSrc});
+    clients.push(client);
+    client.on('message', function(data) {
+      if ('startClockSync' in data) {
+	console.log('syncing clock');
+	client.send({'clockSyncServerTime': (new Date()).getTime()});
+      } else if ('start' in data) {
+	console.log('playing music');
 	start();
-    }
+      }
+    });
+  });
+
+  function seconds(n) {
+    return n * 1000;
+  }
+
+  function start() {
+    clients.forEach(function(client) {
+      client.send({'play': (new Date()).getTime() + seconds(5)});
+    });
+  }
+  console.log('awesome. it\'s up.');
 }
